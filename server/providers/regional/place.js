513b8bfdad4f551da432ed348b397535bfc4cbcc
Jenil Patel <chetnapatel084@gmail.com>
2026-09-17 09:28:39 -0500

    brand: identify Nominatim requests as World Eyes View

--- server/providers/regional/place.js
+++ server/providers/regional/place.js
@@ -14,8 +14,8 @@ import {
  */
 const NOMINATIM_HEADERS = Object.freeze({
   'User-Agent':
-    'gods-eye-view/0.1 (+https://github.com/bilawalsidhu/gods-eye-view)',
-  Referer: 'https://github.com/bilawalsidhu/gods-eye-view',
+    'world-eyes-view/0.1 (+https://github.com/dvilrgamerz/world-eyes-views-)',
+  Referer: 'https://github.com/dvilrgamerz/world-eyes-views-',
 });
 
 /**
